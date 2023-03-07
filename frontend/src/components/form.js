a4223d5113d0fb425c96a9ea98b2e8ae419f37fe
Josimar-Vezu <josimarvezuramires@gmail.com>
2023-03-07 14:53:13 -0300

    Criando Front-End

---
+++ frontend/src/components/form.js
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+import styled from "styled-components";
+
+const FormContainer = styled.form`
+display: flex;
+align-items: flex-end;
+gap: 10px;
+flex-wrap: wrap;
+background-color: #fff;
+padding: 20px;
+box-shadow: 0px 0px 5px #ccc;
+border-radius: 5px;
+`;
+
+const Label = styled.label``;
+
+const Form = ({ onEdit }) => {
+    const ref = useRef();
+
+    return (
+        <FormContainer ref={ref}>
+            <InputArea>
+                <Label>Nome</Label>
+                <input name="nome" />
+            </InputArea>
+        </FormContainer>
+
+    );
+};
+
+export default Form;

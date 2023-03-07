af48d961563787160eb3a4bb795684ef9cc68d91
Josimar-Vezu <josimarvezuramires@gmail.com>
2023-03-07 18:00:01 -0300

    Label e Button

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -10,9 +10,33 @@ background-color: #fff;
 padding: 20px;
 box-shadow: 0px 0px 5px #ccc;
 border-radius: 5px;
+height: 40px;
+`;
+
+const InputArea = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const Input = styled.input`
+width: 120px;
+padding: 0 10px;
+border: 1px solid #bbb;
+border-radius: 5px;
+height: 40px;
 `;
 
 const Label = styled.label``;
+
+const Button = styled.button`
+padding: 10px;
+cursor: pointer;
+border-radius: 5px;
+border: none;
+background-color: #2c73d2;
+color: white;
+height: 42px;
+`;
 
 const Form = ({ onEdit }) => {
     const ref = useRef();
@@ -23,6 +47,22 @@ const Form = ({ onEdit }) => {
                 <Label>Nome</Label>
                 <input name="nome" />
             </InputArea>
+
+            <InputArea>
+                <Label>E-Mail</Label>
+                <input name="email" type="email" />
+            </InputArea>
+
+            <InputArea>
+                <Label>Telefone</Label>
+                <input name="fone" />
+            </InputArea>
+
+            <InputArea>
+                <Label>Data de Nascimento</Label>
+                <input name="data_nascimento" type="date" />
+            </InputArea>
+            <button type="submit"> SALVAR</button>
         </FormContainer>
 
     );
